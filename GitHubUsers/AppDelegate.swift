82a72a40635471028b7f315900a6f6400c7b1895
Kirill Dobryakov <dobryakov.kirill@gmail.com>
2020-02-26 22:54:41 +0300

    Initial Commit

---
+++ GitHubUsers/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  GitHubUsers
+//
+//  Created by dk on 26/02/2020.
+//  Copyright © 2020 dk. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
---
+++ GitHubUsers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitHubUsers
+//
+//  Created by dk on 26/02/2020.
+//  Copyright © 2020 dk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
