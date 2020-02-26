2e5d4540ccde4ff4e844a97ee87edd8a5b1a4b47
Kirill Dobryakov <dobryakov.kirill@gmail.com>
2020-02-26 23:56:28 +0300

    removed stroyboard

--- GitHubUsers/AppDelegate.swift
+++ GitHubUsers/AppDelegate.swift
@@ -10,11 +10,16 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+
+        let homeViewController = UIViewController()
+
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = homeViewController
+        window?.makeKeyAndVisible()
         return true
     }
 
 }
-
